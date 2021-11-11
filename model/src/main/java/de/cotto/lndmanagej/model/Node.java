6ad011696bf76e8aaa6f6c132f95e7fb4033d9a1
Carsten Otto <git@c-otto.de>
2021-11-11 19:45:26 +0100

    add hardcoded aliases

--- model/src/main/java/de/cotto/lndmanagej/model/Node.java
+++ model/src/main/java/de/cotto/lndmanagej/model/Node.java
@@ -1,11 +1,23 @@
 package de.cotto.lndmanagej.model;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Map;
 import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
-public record Node(String alias, int lastUpdate, Pubkey pubkey) implements Comparable<Node> {
+public record Node(@Nonnull String alias, int lastUpdate, @Nonnull Pubkey pubkey) implements Comparable<Node> {
+    private static final Map<Pubkey, String> HARDCODED_ALIASES = Map.of(
+            Pubkey.create("02f72978d40efeffca537139ad6ac9f09970c000a2dbc0d7aa55a71327c4577a80"), "Chivo IBEX_a0",
+            Pubkey.create("037cc5f9f1da20ac0d60e83989729a204a33cc2d8e80438969fadf35c1c5f1233b"), "BlueWallet"
+    );
+
+    public Node(String alias, int lastUpdate, Pubkey pubkey) {
+        this.alias = HARDCODED_ALIASES.getOrDefault(pubkey, alias);
+        this.lastUpdate = lastUpdate;
+        this.pubkey = pubkey;
+    }
 
     public static Builder builder() {
         return new Builder();
@@ -41,7 +53,6 @@ public record Node(String alias, int lastUpdate, Pubkey pubkey) implements Compa
     public static class Builder {
         @Nullable
         private String alias;
-
         private int lastUpdate;
 
         @Nullable
@@ -68,5 +79,6 @@ public record Node(String alias, int lastUpdate, Pubkey pubkey) implements Compa
         public Node build() {
             return new Node(requireNonNull(alias), lastUpdate, requireNonNull(pubkey));
         }
+
     }
 }
