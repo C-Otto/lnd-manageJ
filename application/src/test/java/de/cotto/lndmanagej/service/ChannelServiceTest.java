66a1f809d1aa8058922b1460dc93b74a18976b37
Carsten Otto <git@c-otto.de>
2021-11-15 08:52:32 +0100

    add test

--- application/src/test/java/de/cotto/lndmanagej/service/ChannelServiceTest.java
+++ application/src/test/java/de/cotto/lndmanagej/service/ChannelServiceTest.java
@@ -15,6 +15,7 @@ import java.util.Set;
 import static de.cotto.lndmanagej.model.BalanceInformationFixtures.BALANCE_INFORMATION;
 import static de.cotto.lndmanagej.model.ChannelIdFixtures.CHANNEL_ID_2;
 import static de.cotto.lndmanagej.model.LocalChannelFixtures.LOCAL_CHANNEL;
+import static de.cotto.lndmanagej.model.LocalChannelFixtures.LOCAL_CHANNEL_2;
 import static de.cotto.lndmanagej.model.LocalChannelFixtures.LOCAL_CHANNEL_3;
 import static de.cotto.lndmanagej.model.PubkeyFixtures.PUBKEY;
 import static de.cotto.lndmanagej.model.PubkeyFixtures.PUBKEY_2;
@@ -44,5 +45,11 @@ class ChannelServiceTest {
         when(grpcChannels.getChannels()).thenReturn(Set.of(LOCAL_CHANNEL, localChannel2, LOCAL_CHANNEL_3));
         assertThat(channelService.getOpenChannelsWith(PUBKEY_2))
                 .containsExactlyInAnyOrder(LOCAL_CHANNEL, LOCAL_CHANNEL_3);
+    }
+
+    @Test
+    void getOpenChannels() {
+        when(grpcChannels.getChannels()).thenReturn(Set.of(LOCAL_CHANNEL, LOCAL_CHANNEL_2));
+        assertThat(channelService.getOpenChannels()).containsExactlyInAnyOrder(LOCAL_CHANNEL, LOCAL_CHANNEL_2);
     }
 }
