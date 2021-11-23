a0659f48f62a1562292f8908bc1b244896730417
Carsten Otto <git@c-otto.de>
2021-11-23 11:57:30 +0100

    add default constructor for JPA

--- statistics/src/main/java/de/cotto/lndmanagej/statistics/persistence/StatisticsId.java
+++ statistics/src/main/java/de/cotto/lndmanagej/statistics/persistence/StatisticsId.java
@@ -1,6 +1,20 @@
 package de.cotto.lndmanagej.statistics.persistence;
 
+import javax.annotation.Nullable;
+import java.io.Serial;
 import java.io.Serializable;
 
-public record StatisticsId(Long channelId, long timestamp) implements Serializable {
+@SuppressWarnings("unused")
+public final class StatisticsId implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 0L;
+
+    @Nullable
+    private Long channelId;
+
+    private long timestamp;
+
+    public StatisticsId() {
+        // for JPA
+    }
 }
