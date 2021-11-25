7e2153159bcf285832fb135b26d7f81ba11b7452
Carsten Otto <git@c-otto.de>
2021-11-25 19:49:24 +0100

    add test

--- web/src/test/java/de/cotto/lndmanagej/controller/StatusControllerTest.java
+++ web/src/test/java/de/cotto/lndmanagej/controller/StatusControllerTest.java
@@ -15,7 +15,9 @@ import java.util.List;
 import java.util.Set;
 
 import static de.cotto.lndmanagej.model.LocalOpenChannelFixtures.LOCAL_OPEN_CHANNEL;
+import static de.cotto.lndmanagej.model.LocalOpenChannelFixtures.LOCAL_OPEN_CHANNEL_2;
 import static de.cotto.lndmanagej.model.LocalOpenChannelFixtures.LOCAL_OPEN_CHANNEL_TO_NODE_3;
+import static de.cotto.lndmanagej.model.PubkeyFixtures.PUBKEY_2;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
@@ -56,5 +58,11 @@ class StatusControllerTest {
                 List.of(LOCAL_OPEN_CHANNEL.getRemotePubkey(), LOCAL_OPEN_CHANNEL_TO_NODE_3.getRemotePubkey());
         assertThat(statusController.getPubkeysForOpenChannels()).isEqualTo(new PubkeysDto(expectedPubkeys));
         verify(metrics).mark(argThat(name -> name.endsWith(".getPubkeysForOpenChannels")));
+    }
+
+    @Test
+    void getPeerPubkeys_without_no_duplicates() {
+        when(channelService.getOpenChannels()).thenReturn(Set.of(LOCAL_OPEN_CHANNEL, LOCAL_OPEN_CHANNEL_2));
+        assertThat(statusController.getPubkeysForOpenChannels().pubkeys()).containsExactly(PUBKEY_2.toString());
     }
 }
