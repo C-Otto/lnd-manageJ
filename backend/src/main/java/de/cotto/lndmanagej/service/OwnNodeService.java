bd0cdf0d980d9ac3d512b6d00abfb1eed82f2fd4
Carsten Otto <git@c-otto.de>
2021-11-27 12:00:21 +0100

    cache block height, refresh status caches

--- backend/src/main/java/de/cotto/lndmanagej/service/OwnNodeService.java
+++ backend/src/main/java/de/cotto/lndmanagej/service/OwnNodeService.java
@@ -6,15 +6,23 @@ import de.cotto.lndmanagej.grpc.GrpcGetInfo;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Objects;
 
 @Component
 public class OwnNodeService {
+    private static final Duration CACHE_REFRESH = Duration.ofSeconds(15);
     private static final Duration CACHE_EXPIRY = Duration.ofSeconds(30);
 
     private final GrpcGetInfo grpcGetInfo;
     private final LoadingCache<Object, Boolean> syncedToChainCache = new CacheBuilder()
+            .withRefresh(CACHE_REFRESH)
             .withExpiry(CACHE_EXPIRY)
             .build(this::isSyncedToChainWithoutCache);
+
+    private final LoadingCache<Object, Integer> blockHeightCache = new CacheBuilder()
+            .withRefresh(CACHE_REFRESH)
+            .withExpiry(CACHE_EXPIRY)
+            .build(this::getBlockHeightWithoutCache);
 
     public OwnNodeService(GrpcGetInfo grpcGetInfo) {
         this.grpcGetInfo = grpcGetInfo;
@@ -25,6 +33,10 @@ public class OwnNodeService {
     }
 
     public int getBlockHeight() {
+        return Objects.requireNonNull(blockHeightCache.get(""));
+    }
+
+    private int getBlockHeightWithoutCache() {
         return grpcGetInfo.getBlockHeight().orElseThrow();
     }
 
