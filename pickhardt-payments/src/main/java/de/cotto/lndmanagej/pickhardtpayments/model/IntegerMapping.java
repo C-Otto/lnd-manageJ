7c673501c02586f06b85875c0afc9e8b0024a348
Carsten Otto <git@c-otto.de>
2022-04-02 17:01:28 +0200

    use specialized maps for (pubkey) integer mapping

--- pickhardt-payments/src/main/java/de/cotto/lndmanagej/pickhardtpayments/model/IntegerMapping.java
+++ pickhardt-payments/src/main/java/de/cotto/lndmanagej/pickhardtpayments/model/IntegerMapping.java
@@ -1,13 +1,14 @@
 package de.cotto.lndmanagej.pickhardtpayments.model;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import org.eclipse.collections.impl.map.mutable.primitive.IntObjectHashMap;
+import org.eclipse.collections.impl.map.mutable.primitive.ObjectIntHashMap;
+
 import java.util.Objects;
 
 public class IntegerMapping<K> {
     private static final int INITIAL_CAPACITY = 10_000;
-    private final Map<K, Integer> mapping = new LinkedHashMap<>(INITIAL_CAPACITY);
-    private final Map<Integer, K> reverseMapping = new LinkedHashMap<>(INITIAL_CAPACITY);
+    private final ObjectIntHashMap<K> mapping = new ObjectIntHashMap<>(INITIAL_CAPACITY);
+    private final IntObjectHashMap<K> reverseMapping = new IntObjectHashMap<>(INITIAL_CAPACITY);
     private int counter;
 
     public IntegerMapping() {
@@ -15,9 +16,11 @@ public class IntegerMapping<K> {
     }
 
     public int getMappedInteger(K key) {
-        Integer integer = mapping.computeIfAbsent(key, k -> counter++);
-        reverseMapping.put(integer, key);
-        return integer;
+        return mapping.getIfAbsentPut(key, () -> {
+            int value = counter++;
+            reverseMapping.put(value, key);
+            return value;
+        });
     }
 
     public K getKey(int integer) {
