654e6a559a6a4e57db93f29a962b47aca9681869
Carsten Otto <git@c-otto.de>
2022-05-10 19:30:32 +0200

    cleanup

--- web/src/integrationTest/java/de/cotto/lndmanagej/ui/NodeDetailsControllerIT.java
+++ web/src/integrationTest/java/de/cotto/lndmanagej/ui/NodeDetailsControllerIT.java
@@ -1,6 +1,6 @@
 package de.cotto.lndmanagej.ui;
 
-import de.cotto.lndmanagej.controller.ChannelIdConverter;
+import de.cotto.lndmanagej.model.ChannelIdResolver;
 import de.cotto.lndmanagej.ui.controller.NodeDetailsController;
 import de.cotto.lndmanagej.ui.page.PageService;
 import de.cotto.lndmanagej.ui.page.node.NodeDetailsPage;
@@ -9,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static de.cotto.lndmanagej.ui.model.NodeDetailsDtoFixture.NODE_DETAILS_DTO;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = NodeDetailsController.class)
@@ -25,14 +25,14 @@ class NodeDetailsControllerIT {
     @MockBean
     private PageService pageService;
 
-    @SuppressWarnings("unused")
     @MockBean
-    private ChannelIdConverter channelIdConverter;
+    @SuppressWarnings("unused")
+    private ChannelIdResolver channelIdResolver;
 
     @Test
     void testNodeDetailsPage() throws Exception {
-        given(this.pageService.nodeDetails(any())).willReturn(new NodeDetailsPage(NODE_DETAILS_DTO));
-        mockMvc.perform(MockMvcRequestBuilders.get("/node/" + NODE_DETAILS_DTO.node().toString()))
+        when(pageService.nodeDetails(any())).thenReturn(new NodeDetailsPage(NODE_DETAILS_DTO));
+        mockMvc.perform(get("/node/" + NODE_DETAILS_DTO.node().toString()))
                 .andExpect(status().isOk());
     }
 }
