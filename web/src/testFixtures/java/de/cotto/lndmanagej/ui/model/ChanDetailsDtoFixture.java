ffe0bdd530158222ae72ff392f1c58830ca19e74
Carsten Otto <git@c-otto.de>
2022-05-10 19:24:49 +0200

    use static imports

--- web/src/testFixtures/java/de/cotto/lndmanagej/ui/model/ChanDetailsDtoFixture.java
+++ web/src/testFixtures/java/de/cotto/lndmanagej/ui/model/ChanDetailsDtoFixture.java
@@ -6,24 +6,24 @@ import de.cotto.lndmanagej.controller.dto.FlowReportDto;
 import de.cotto.lndmanagej.controller.dto.OnChainCostsDto;
 import de.cotto.lndmanagej.controller.dto.PoliciesDto;
 import de.cotto.lndmanagej.controller.dto.RebalanceReportDto;
-import de.cotto.lndmanagej.model.ChannelIdFixtures;
 import de.cotto.lndmanagej.model.FeeReportFixtures;
 import de.cotto.lndmanagej.model.FlowReportFixtures;
 import de.cotto.lndmanagej.model.OpenInitiator;
-import de.cotto.lndmanagej.model.PubkeyFixtures;
 import de.cotto.lndmanagej.model.RebalanceReportFixtures;
 import de.cotto.lndmanagej.model.warnings.ChannelWarningsFixtures;
 import de.cotto.lndmanagej.ui.dto.ChanDetailsDto;
 
 import static de.cotto.lndmanagej.model.BalanceInformationFixtures.BALANCE_INFORMATION;
+import static de.cotto.lndmanagej.model.ChannelIdFixtures.CHANNEL_ID;
 import static de.cotto.lndmanagej.model.OnChainCostsFixtures.ON_CHAIN_COSTS;
 import static de.cotto.lndmanagej.model.PolicyFixtures.POLICIES_FOR_LOCAL_CHANNEL;
+import static de.cotto.lndmanagej.model.PubkeyFixtures.PUBKEY;
 
 public class ChanDetailsDtoFixture {
 
     public static final ChanDetailsDto CHAN_DETAILS_DTO = new ChanDetailsDto(
-            ChannelIdFixtures.CHANNEL_ID,
-            PubkeyFixtures.PUBKEY,
+            CHANNEL_ID,
+            PUBKEY,
             "Albert",
             OpenInitiator.LOCAL,
             BalanceInformationDto.createFromModel(BALANCE_INFORMATION),
