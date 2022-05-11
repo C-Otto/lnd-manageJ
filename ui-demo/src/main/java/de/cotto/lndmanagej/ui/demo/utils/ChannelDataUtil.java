05f5210f003da165bd4eff810de4050799e8de9d
danielbroll <danielbroll@protonmail.com>
2022-05-11 11:07:16 +0200

    add good-looking demo channels

--- ui-demo/src/main/java/de/cotto/lndmanagej/ui/demo/utils/ChannelDataUtil.java
+++ ui-demo/src/main/java/de/cotto/lndmanagej/ui/demo/utils/ChannelDataUtil.java
@@ -5,88 +5,41 @@ import de.cotto.lndmanagej.controller.dto.PoliciesDto;
 import de.cotto.lndmanagej.model.BalanceInformation;
 import de.cotto.lndmanagej.model.ChannelId;
 import de.cotto.lndmanagej.model.Coins;
+import de.cotto.lndmanagej.model.PoliciesForLocalChannel;
+import de.cotto.lndmanagej.model.Policy;
 import de.cotto.lndmanagej.model.Pubkey;
 import de.cotto.lndmanagej.ui.dto.OpenChannelDto;
 
-import static de.cotto.lndmanagej.model.ChannelIdFixtures.CHANNEL_ID_2;
-import static de.cotto.lndmanagej.model.ChannelIdFixtures.CHANNEL_ID_3;
-import static de.cotto.lndmanagej.model.ChannelIdFixtures.CHANNEL_ID_4;
-import static de.cotto.lndmanagej.model.ChannelIdFixtures.CHANNEL_ID_5;
-import static de.cotto.lndmanagej.model.PolicyFixtures.POLICIES_FOR_LOCAL_CHANNEL;
+import java.util.Random;
 
 public class ChannelDataUtil {
 
-    public static final long MILLION = 1_000_000;
+    public static OpenChannelDto createOpenChannel(String compactChannelId, String alias, String pubkey, long local, long remote) {
+        ChannelId channelId = ChannelId.fromCompactForm(compactChannelId);
+        return new OpenChannelDto(
+                channelId,
+                alias,
+                Pubkey.create(pubkey),
+                PoliciesDto.createFromModel(derivePolicies(channelId)),
+                BalanceInformationDto.createFromModel(new BalanceInformation(
+                        Coins.ofSatoshis(local),
+                        Coins.ofSatoshis(200),
+                        Coins.ofSatoshis(remote),
+                        Coins.ofSatoshis(500)
+                )));
+    }
 
-    public static final OpenChannelDto ACINQ = new OpenChannelDto(
-            CHANNEL_ID_2,
-            "ACINQ",
-            Pubkey.create("03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f"),
-            PoliciesDto.createFromModel(POLICIES_FOR_LOCAL_CHANNEL),
-            BalanceInformationDto.createFromModel(new BalanceInformation(
-                    Coins.ofSatoshis(MILLION * 5),
-                    Coins.ofSatoshis(200),
-                    Coins.ofSatoshis(MILLION * 5),
-                    Coins.ofSatoshis(500)
-            )));
+    private static PoliciesForLocalChannel derivePolicies(ChannelId channelId) {
+        Random generator = new Random(channelId.getShortChannelId());
+        return new PoliciesForLocalChannel(derivePolicy(generator), derivePolicy(generator));
+    }
 
-    public static final OpenChannelDto ACINQ2 = new OpenChannelDto(
-            ChannelId.fromCompactForm("799999x456x7"),
-            "ACINQ",
-            Pubkey.create("03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f"),
-            PoliciesDto.createFromModel(POLICIES_FOR_LOCAL_CHANNEL),
-            BalanceInformationDto.createFromModel(new BalanceInformation(
-                    Coins.ofSatoshis(100_000),
-                    Coins.ofSatoshis(250),
-                    Coins.ofSatoshis(MILLION * 10),
-                    Coins.ofSatoshis(250)
-            )));
+    private static Policy derivePolicy(Random generator) {
+        long feeRate = generator.nextLong(100) * 10;
+        Coins baseFee = Coins.ofMilliSatoshis(generator.nextLong(2) * 1000);
+        boolean enabled = generator.nextInt(10) == 0;
+        int timeLockDelta = (generator.nextInt(5) + 1) * 10;
+        return new Policy(feeRate, baseFee, enabled, timeLockDelta);
+    }
 
-    public static final OpenChannelDto WOS = new OpenChannelDto(
-            CHANNEL_ID_3,
-            "WalletOfSatoshi.com",
-            Pubkey.create("035e4ff418fc8b5554c5d9eea66396c227bd429a3251c8cbc711002ba215bfc226"),
-            PoliciesDto.createFromModel(POLICIES_FOR_LOCAL_CHANNEL),
-            BalanceInformationDto.createFromModel(new BalanceInformation(
-                    Coins.ofSatoshis(MILLION * 2),
-                    Coins.ofSatoshis(400),
-                    Coins.ofSatoshis(MILLION * 10),
-                    Coins.ofSatoshis(500)
-            )));
-
-    public static final OpenChannelDto WOS2 = new OpenChannelDto(
-            CHANNEL_ID_3,
-            "WalletOfSatoshi.com",
-            Pubkey.create("035e4ff418fc8b5554c5d9eea66396c227bd429a3251c8cbc711002ba215bfc226"),
-            PoliciesDto.createFromModel(POLICIES_FOR_LOCAL_CHANNEL),
-            BalanceInformationDto.createFromModel(new BalanceInformation(
-                    Coins.ofSatoshis(MILLION * 10),
-                    Coins.ofSatoshis(400),
-                    Coins.ofSatoshis(100_000),
-                    Coins.ofSatoshis(500)
-            )));
-
-    public static final OpenChannelDto BCASH = new OpenChannelDto(
-            CHANNEL_ID_4,
-            "BCash_Is_Trash",
-            Pubkey.create("0298f6074a454a1f5345cb2a7c6f9fce206cd0bf675d177cdbf0ca7508dd28852f"),
-            PoliciesDto.createFromModel(POLICIES_FOR_LOCAL_CHANNEL),
-            BalanceInformationDto.createFromModel(new BalanceInformation(
-                    Coins.ofSatoshis(MILLION * 10),
-                    Coins.ofSatoshis(400),
-                    Coins.ofSatoshis(MILLION * 20),
-                    Coins.ofSatoshis(500)
-            )));
-
-    public static final OpenChannelDto C_OTTO = new OpenChannelDto(
-            CHANNEL_ID_5,
-            "c-otto.de",
-            Pubkey.create("027ce055380348d7812d2ae7745701c9f93e70c1adeb2657f053f91df4f2843c71"),
-            PoliciesDto.createFromModel(POLICIES_FOR_LOCAL_CHANNEL),
-            BalanceInformationDto.createFromModel(new BalanceInformation(
-                    Coins.ofSatoshis(MILLION * 20),
-                    Coins.ofSatoshis(400),
-                    Coins.ofSatoshis(MILLION * 10),
-                    Coins.ofSatoshis(500)
-            )));
 }
