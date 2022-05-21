8d3a32b6a08b8165c86f69f57446065a5481e8fb
danielbroll <danielbroll@protonmail.com>
2022-05-21 14:41:18 +0200

    dashboard/nodes page: group nodes by online/offline & sort nodes by alias

--- ui/src/main/java/de/cotto/lndmanagej/ui/dto/NodeDto.java
+++ ui/src/main/java/de/cotto/lndmanagej/ui/dto/NodeDto.java
@@ -1,4 +1,20 @@
 package de.cotto.lndmanagej.ui.dto;
 
+import java.io.Serializable;
+import java.util.Comparator;
+
 public record NodeDto(String pubkey, String alias, boolean online) {
+
+    public static class OnlineStatusAndAliasComparator implements Comparator<NodeDto>, Serializable {
+
+        @Override
+        public int compare(NodeDto node1, NodeDto node2) {
+            boolean bothOffline = !node1.online && !node2.online;
+            boolean bothOnline = node1.online && node2.online;
+            if (bothOffline || bothOnline) {
+                return node1.alias.compareToIgnoreCase(node2.alias);
+            }
+            return node1.online ? 1 : -1;
+        }
+    }
 }
---
+++ ui/src/test/java/de/cotto/lndmanagej/ui/dto/NodeDtoTest.java
@@ -0,0 +1,40 @@
+package de.cotto.lndmanagej.ui.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static de.cotto.lndmanagej.model.PubkeyFixtures.PUBKEY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NodeDtoTest {
+
+    public static final String ALICE = "alice";
+    public static final String BOB = "bob";
+
+    private static final NodeDto.OnlineStatusAndAliasComparator COMPARATOR
+            = new NodeDto.OnlineStatusAndAliasComparator();
+
+    @Test
+    void compare_differentStatus_byStatus() {
+        NodeDto alice = node(ALICE, true);
+        NodeDto bob = node(BOB, false);
+        assertThat(COMPARATOR.compare(alice, bob)).isPositive();
+    }
+
+    @Test
+    void compare_bothOnline_byAlias() {
+        NodeDto alice = node(ALICE, true);
+        NodeDto bob = node(BOB, true);
+        assertThat(COMPARATOR.compare(alice, bob)).isNegative();
+    }
+
+    @Test
+    void compare_bothOffline_byAlias() {
+        NodeDto alice = node(ALICE, false);
+        NodeDto bob = node(BOB, false);
+        assertThat(COMPARATOR.compare(alice, bob)).isNegative();
+    }
+
+    private NodeDto node(String alias, boolean online) {
+        return new NodeDto(PUBKEY.toString(), alias, online);
+    }
+}
