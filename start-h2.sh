1d8faed912994637dd43158e4f6db901d9ab064e
Carsten Otto <git@c-otto.de>
2022-07-09 10:51:39 +0200

    add support for h2
    fixes #3

---
+++ start-h2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ "x$TERM" == "xdumb" ];
+then
+  export TERM="xterm"
+fi
+./gradlew application:bootJar && clear && java -Dspring.profiles.active=h2 -jar application/build/libs/application-boot.jar
