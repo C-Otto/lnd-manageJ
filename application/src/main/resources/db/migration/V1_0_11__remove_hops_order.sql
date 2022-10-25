7742de845833522287be6b479bd18bda92063967
Carsten Otto <git@c-otto.de>
2022-10-25 23:01:08 +0200

    remove unused column, cleanup indexes

---
+++ application/src/main/resources/db/migration/V1_0_11__remove_hops_order.sql
@@ -0,0 +1,5 @@
+drop index channel_id_and_hop_order_index;
+alter table PAYMENT_ROUTE_HOPS drop column HOPS_ORDER;
+create index if not exists channel_id_and_first_index
+    on payment_route_hops (channel_id, first);
+drop index channel_id_index;
