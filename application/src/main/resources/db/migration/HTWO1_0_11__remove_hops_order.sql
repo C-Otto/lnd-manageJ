548deb81d2affefad63d45a22abea9c6107fd045
Carsten Otto <git@c-otto.de>
2022-10-26 10:16:17 +0200

    fix migration script for H2

--- application/src/main/resources/db/migration/HTWO1_0_11__remove_hops_order.sql
+++ application/src/main/resources/db/migration/HTWO1_0_11__remove_hops_order.sql
@@ -1,4 +1,5 @@
 drop index channel_id_and_hop_order_index;
+alter table PAYMENT_ROUTE_HOPS drop constraint constraint_4;
 alter table PAYMENT_ROUTE_HOPS drop column HOPS_ORDER;
 create index if not exists channel_id_and_first_index
     on payment_route_hops (channel_id, first);
