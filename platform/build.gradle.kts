8957c3f0ed5c4ba68408378ef3c637e52e54c59a
Carsten Otto <git@c-otto.de>
2022-11-24 16:00:36 +0100

    update spring boot

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -9,7 +9,7 @@ javaPlatform {
 }
 
 dependencies {
-    val springBootVersion = "2.7.5"
+    val springBootVersion = "2.7.6"
     val grpcVersion = "1.50.2"
     api(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.4"))
     api(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
