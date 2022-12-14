21ea70e8a166365ac4bf14075034ba05e05aaa2e
Carsten Otto <git@c-otto.de>
2022-12-14 22:05:16 +0100

    update webjars-locator

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
         api("org.ini4j:ini4j:0.5.4")
         api("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
         api("org.webjars:bootstrap:5.2.3")
-        api("org.webjars:webjars-locator:0.45")
+        api("org.webjars:webjars-locator:0.46")
         api("com.github.valfirst:slf4j-test:2.6.1")
     }
 }
