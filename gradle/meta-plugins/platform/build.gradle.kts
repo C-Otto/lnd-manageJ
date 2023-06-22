8560fb32c57d7d5853e873d79c5ef52059381cf6
Carsten Otto <git@c-otto.de>
2023-06-22 19:52:14 +0200

    update libraries

--- gradle/meta-plugins/platform/build.gradle.kts
+++ gradle/meta-plugins/platform/build.gradle.kts
@@ -9,15 +9,15 @@ javaPlatform {
 }
 
 dependencies {
-    val springBootVersion = "3.0.6"
-    val grpcVersion = "1.51.1"
+    val springBootVersion = "3.1.1"
+    val grpcVersion = "1.56.0"
 
-    api(platform("org.springframework.cloud:spring-cloud-dependencies:2022.0.0"))
+    api(platform("org.springframework.cloud:spring-cloud-dependencies:2022.0.3"))
     api(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
 
     constraints {
-        api("com.google.ortools:ortools-java:9.5.2237")
-        api("com.google.protobuf:protobuf-gradle-plugin:0.9.2")
+        api("com.google.ortools:ortools-java:9.6.2534")
+        api("com.google.protobuf:protobuf-gradle-plugin:0.9.3")
         api("io.grpc:grpc-netty:$grpcVersion")
         api("io.grpc:grpc-protobuf:$grpcVersion")
         api("io.grpc:grpc-stub:$grpcVersion")
