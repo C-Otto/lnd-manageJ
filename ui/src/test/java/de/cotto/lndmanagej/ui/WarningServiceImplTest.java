234c950c99d844fda59c9d026810d28bbcbc7bc7
Carsten Otto <git@c-otto.de>
2023-08-03 17:07:27 +0200

    refactor test

--- ui/src/test/java/de/cotto/lndmanagej/ui/WarningServiceImplTest.java
+++ ui/src/test/java/de/cotto/lndmanagej/ui/WarningServiceImplTest.java
@@ -5,6 +5,7 @@ import de.cotto.lndmanagej.service.ChannelWarningsService;
 import de.cotto.lndmanagej.service.NodeService;
 import de.cotto.lndmanagej.service.NodeWarningsService;
 import de.cotto.lndmanagej.ui.dto.warning.DashboardWarningDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,9 +25,11 @@ import static de.cotto.lndmanagej.model.warnings.ChannelWarningFixtures.CHANNEL_
 import static de.cotto.lndmanagej.model.warnings.ChannelWarningFixtures.CHANNEL_NUM_UPDATES_WARNING;
 import static de.cotto.lndmanagej.model.warnings.ChannelWarningsFixtures.CHANNEL_WARNINGS;
 import static de.cotto.lndmanagej.model.warnings.NodeWarningsFixtures.NODE_WARNINGS;
+import static de.cotto.lndmanagej.model.warnings.NodeWarningsFixtures.NODE_WARNINGS_2;
 import static de.cotto.lndmanagej.ui.dto.warning.ChannelWarningDtoFixture.CHANNEL_WARNING_DTO;
 import static de.cotto.lndmanagej.ui.dto.warning.ChannelWarningDtoFixture.CHANNEL_WARNING_DTO_2;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,6 +39,8 @@ class WarningServiceImplTest {
             "Node changed between online and offline 123 times in the past 7 days",
             "Node has been online 51% in the past 14 days"
     );
+    private static final String ALIAS_FOR_NODE = "Node";
+    private static final String ALIAS_FOR_PUBKEY_2 = "Another Node";
 
     @InjectMocks
     private WarningServiceImpl warningService;
@@ -49,21 +54,22 @@ class WarningServiceImplTest {
     @Mock
     private ChannelWarningsService channelWarningsService;
 
-    @Test
-    void getWarnings_noWarnings_noWarnings() {
-        when(nodeWarningsService.getNodeWarnings()).thenReturn(Map.of());
-        when(channelWarningsService.getChannelWarnings()).thenReturn(Map.of());
+    @BeforeEach
+    void setUp() {
+        lenient().when(nodeService.getAlias(PUBKEY_2)).thenReturn(ALIAS_FOR_PUBKEY_2);
+    }
 
+    @Test
+    void getWarnings_empty() {
         assertThat(warningService.getWarnings()).isEmpty();
     }
 
     @Test
-    void getWarnings_onlyNodeWarnings() {
+    void getWarnings_only_node_warnings() {
         when(nodeWarningsService.getNodeWarnings()).thenReturn(Map.of(NODE, NODE_WARNINGS));
-        when(channelWarningsService.getChannelWarnings()).thenReturn(Map.of());
 
         assertThat(warningService.getWarnings()).containsExactly(new DashboardWarningDto(
-                "Node",
+                ALIAS_FOR_NODE,
                 PUBKEY,
                 THREE_NODE_WARNINGS,
                 List.of()
@@ -71,17 +77,25 @@ class WarningServiceImplTest {
     }
 
     @Test
-    void getWarnings_onlyChannelWarnings() {
-        when(nodeWarningsService.getNodeWarnings()).thenReturn(Map.of());
+    void getWarnings_node_warnings_sorted_by_pubkey() {
+        when(nodeWarningsService.getNodeWarnings()).thenReturn(Map.of(
+                NODE_2, NODE_WARNINGS_2,
+                NODE, NODE_WARNINGS
+        ));
+
+        assertThat(warningService.getWarnings()).map(DashboardWarningDto::pubkey)
+                .containsExactly(PUBKEY, PUBKEY_2);
+    }
+
+    @Test
+    void getWarnings_only_channel_warnings() {
         when(channelWarningsService.getChannelWarnings()).thenReturn(Map.of(
                 LOCAL_OPEN_CHANNEL, new ChannelWarnings(CHANNEL_NUM_UPDATES_WARNING),
                 LOCAL_OPEN_CHANNEL_2, new ChannelWarnings(CHANNEL_BALANCE_FLUCTUATION_WARNING))
         );
-        String alias = "Another Node";
-        when(nodeService.getAlias(PUBKEY_2)).thenReturn(alias);
 
         assertThat(warningService.getWarnings()).containsExactly(new DashboardWarningDto(
-                alias,
+                ALIAS_FOR_PUBKEY_2,
                 PUBKEY_2,
                 List.of(),
                 List.of(CHANNEL_WARNING_DTO, CHANNEL_WARNING_DTO_2)
@@ -89,18 +103,33 @@ class WarningServiceImplTest {
     }
 
     @Test
-    void getWarnings_nodeAndChannelWarnings_combined() {
+    void getWarnings_channel_warnings_sorted_by_channel_id() {
+        when(channelWarningsService.getChannelWarnings()).thenReturn(Map.of(
+                LOCAL_OPEN_CHANNEL_2, new ChannelWarnings(CHANNEL_BALANCE_FLUCTUATION_WARNING),
+                LOCAL_OPEN_CHANNEL, new ChannelWarnings(CHANNEL_NUM_UPDATES_WARNING))
+        );
+
+        assertThat(warningService.getWarnings()).containsExactly(new DashboardWarningDto(
+                ALIAS_FOR_PUBKEY_2,
+                PUBKEY_2,
+                List.of(),
+                List.of(CHANNEL_WARNING_DTO, CHANNEL_WARNING_DTO_2)
+        ));
+    }
+
+    @Test
+    void getWarnings_node_and_channel_warnings_combined() {
         when(nodeWarningsService.getNodeWarnings()).thenReturn(Map.of(NODE, NODE_WARNINGS, NODE_2, NODE_WARNINGS));
         when(channelWarningsService.getChannelWarnings()).thenReturn(Map.of(LOCAL_OPEN_CHANNEL, CHANNEL_WARNINGS));
 
         DashboardWarningDto nodeWarning = new DashboardWarningDto(
-                "Node",
+                ALIAS_FOR_NODE,
                 PUBKEY,
                 THREE_NODE_WARNINGS,
                 List.of()
         );
         DashboardWarningDto anotherNodeWarning = new DashboardWarningDto(
-                "Another Node",
+                ALIAS_FOR_PUBKEY_2,
                 PUBKEY_2,
                 THREE_NODE_WARNINGS,
                 List.of(CHANNEL_WARNING_DTO)
