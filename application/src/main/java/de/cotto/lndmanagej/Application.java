db02e72a1bc222af6028255a5d683f1e2a92b6b1
Carsten Otto <git@c-otto.de>
2024-06-07 12:55:45 +0200

    Revert "work around flyway lock issue"
    This reverts commit 7887ae557212514077827e243506814834abf2ff.

--- application/src/main/java/de/cotto/lndmanagej/Application.java
+++ application/src/main/java/de/cotto/lndmanagej/Application.java
@@ -2,27 +2,14 @@ package de.cotto.lndmanagej;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Map;
 
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
 public class Application {
-    public Application() {
-        // default constructor
-    }
-
     public static void main(String[] arguments) {
         SpringApplication.run(Application.class, arguments);
-    }
-
-    @Bean
-    public FlywayConfigurationCustomizer flywayCustomizer() {
-        return configuration -> configuration.configuration(Map.of("flyway.postgresql.transactional.lock", "false"));
     }
 }
