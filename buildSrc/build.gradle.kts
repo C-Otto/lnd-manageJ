dcd4f90780c92ec963682612c3372c2eed89649e
Carsten Otto <git@c-otto.de>
2024-06-07 11:18:17 +0200

    update libraries per conventions plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 
 dependencies {
     implementation(platform("de.c-otto.lndmanagej:platform"))
-    implementation("de.c-otto:java-conventions:2024.02.13_2")
+    implementation("de.c-otto:java-conventions:2024.06.07")
     implementation("org.springframework.boot:spring-boot-gradle-plugin")
     implementation("com.google.protobuf:protobuf-gradle-plugin")
 }
