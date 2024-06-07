0a8f6005e6ba690e494bbd4e93293509e9d9be9c
Carsten Otto <git@c-otto.de>
2024-06-07 12:57:23 +0200

    suppress warning

--- application/src/main/java/de/cotto/lndmanagej/Application.java
+++ application/src/main/java/de/cotto/lndmanagej/Application.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
+@SuppressWarnings("PMD.UseUtilityClass")
 public class Application {
     public static void main(String[] arguments) {
         SpringApplication.run(Application.class, arguments);
